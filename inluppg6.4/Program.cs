acd14db41893b1b0ea6afab0d3486865ccda8ae8
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-16 10:54:11 +0100

    Add project files.

---
+++ inluppg6.4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace inluppg6_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en bas, sedan en exponent");
+            double bas = double.Parse(Console.ReadLine());
+            int exponent = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Exponent(bas, exponent));
+        }
+
+        static double Exponent(double x, int y)
+        {
+            double resultat = x;
+            for (int i = 1; i < y; i++)
+            {
+                resultat = resultat * x;
+            }
+
+            return resultat;
+        }
+    }
+}
